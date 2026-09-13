a28993de310b0129cb903ea9f79b18314a783f10
Arthur Oliveira <artholi321@gmail.com>
2026-09-12 22:52:21 -0300

    feat: finaliza a classe 'GeradorPin'

--- Aula06/src/business/GeradorPin.java
+++ Aula06/src/business/GeradorPin.java
@@ -1,10 +1,36 @@
 package business;
 
-public class GeradorPin extends GeradorSenha{
+public class GeradorPin extends GeradorSenha {
 
     @Override
     public String gerarSenha() {
-        // TODO implementar
-        return null;
+
+        while (true) {
+
+            StringBuilder pin = new StringBuilder();
+
+            for (int i = 0; i < 6; i++) {
+                pin.append(sortearCaractere(NUMEROS));
+            }
+
+            String resultado = pin.toString();
+
+            // variavel q não permite todos os dígitos iguais
+            boolean todosIguais = true;
+
+            for (int i = 1; i < resultado.length(); i++) {
+                if (resultado.charAt(i) != resultado.charAt(0)) {
+                    todosIguais = false;
+                    break;
+                }
+            }
+
+            // nao permite as sequencias especificas
+            boolean sequenciaProibida = resultado.equals("123456") || resultado.equals("654321");
+
+            if (!todosIguais && !sequenciaProibida) {
+                return resultado;
+            }
+        }
     }
 }
